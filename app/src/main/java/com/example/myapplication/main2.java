4333fb8333cfc1ed68cd7db579c01416435f299e
dante flores <dante.kiki569@gmail.com>
2022-10-03 23:47:52 -0300

    actualizacion de vistas

---
+++ app/src/main/java/com/example/myapplication/main2.java
@@ -0,0 +1,13 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class main2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main2);
+    }
+}
